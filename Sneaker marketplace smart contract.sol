3761aa429d64c40a4ec0984a4452ad3fba0f7519
David Beckford <31355452+Dab1010@users.noreply.github.com>
2024-03-14 10:27:40 -0400

    Update Sneaker marketplace smart contract.sol

--- Sneaker marketplace smart contract.sol
+++ Sneaker marketplace smart contract.sol
@@ -1,5 +1,3 @@
-Sneaker marketplace smart contract
-
 pragma solidity ^0.8.0;
 
 contract SneakerMarketplace {
@@ -12,21 +10,18 @@ contract SneakerMarketplace {
         address owner;
         bool forSale;
     }
+
     mapping(address => Sneaker) public sneakers;
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
 
     function addSneakers(string memory brand, string memory model, uint8 size, uint quantity) public {
         require(msg.sender == owner, "Only the owner can add sneakers to the marketplace.");
         for (uint i = 0; i < quantity; i++) {
-            address newSneaker = address(uint(keccak256(abi.encodePacked(now, i))) % 10**20);
-            sneakers[newSneaker].brand = brand;
-            sneakers[newSneaker].model = model;
-            sneakers[newSneaker].size = size;
-            sneakers[newSneaker].owner = msg.sender;
-            sneakers[newSneaker].forSale = true;
+            address newSneaker = address(uint(keccak256(abi.encodePacked(block.timestamp, i))) % 10**20);
+            sneakers[newSneaker] = Sneaker(brand, model, size, msg.sender, true);
         }
     }
 
@@ -38,3 +33,4 @@ contract SneakerMarketplace {
         sneaker.owner.transfer(msg.value);
     }
 }
+
